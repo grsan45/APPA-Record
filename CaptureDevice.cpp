ed4d103eb86d2f863f6d6764d64784d89c709875
grsan45 <grsan45@gmail.com>
2022-04-16 01:01:48 -0400

    image formatttsss

--- CaptureDevice.cpp
+++ CaptureDevice.cpp
@@ -89,16 +89,16 @@ void CaptureDevice::start_write_thread() {
                         if (frame.empty())
                             continue;
 
-                        cv::imwrite(format(image_dir + "/frame-%d.bmp", frame_num), frame);
+                        cv::imwrite(format(image_dir + "/frame-%d.jpg", frame_num), frame);
 
                         frame.release();
                         frame_queue.pop();
                         frame_num++;
-                        printf("wrote frame %d, frame queue size: %lu\n", frame_num, frame_queue.size());
+//                        printf("wrote frame %d, frame queue size: %lu\n", frame_num, frame_queue.size());
                     }
                     printf("Done writing images.\n");
                     for (int i = 0; i < frame_num; i++) {
-                        cv::Mat frame = cv::imread(format(image_dir + "/frame-%d.bmp", i));
+                        cv::Mat frame = cv::imread(format(image_dir + "/frame-%d.jpg", i));
                         writer.write(frame);
                         frame.release();
                     }
